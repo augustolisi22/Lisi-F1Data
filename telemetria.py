198475cfa95628b2aa647b5891aa88df780453d7
augustolisi22 <augustolisi22@gmail.com>
2026-09-02 15:47:44 -0300

    Agrego una funcion formato tiempo que sirve para convertir los minutos en un valor de 2 cifras y para convertir los segundos en un valor float de 6 cifras (ya que en la formula 1 se usan milisegundos). Esto para usarlo mas adelante

--- telemetria.py
+++ telemetria.py
@@ -5,3 +5,12 @@ if not os.path.exists('cache'):
     os.makedirs('cache')
 
 fastf1.Cache.enable_cache('cache')
+
+
+def formato_tiempo(tiempo):
+    try:
+        minutos = int(tiempo.total_seconds() // 60)
+        segundos = tiempo.total_seconds() % 60
+        return f"{minutos:02d}:{segundos:06.3f}"
+    except:
+        return "No hay tiempo disponible"
