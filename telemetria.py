aa89ef8f9ac74f44ac8e271a52aab91b6833d381
augustolisi22 <augustolisi22@gmail.com>
2026-09-02 16:02:53 -0300

    Agrego get_session para ubicar la carrera que quiero analizar, tambien agrego load para que cargue dicha carrera

--- telemetria.py
+++ telemetria.py
@@ -14,3 +14,16 @@ def formato_tiempo(tiempo):
         return f"{minutos:02d}:{segundos:06.3f}"
     except:
         return "No hay tiempo disponible"
+
+
+def comparar_ritmo(piloto1, piloto2, gp, año):
+    try:
+        p1 = piloto1.strip().upper()
+        p2 = piloto2.strip().upper()
+        gran_premio = gp.strip().title()
+
+        sesion = fastf1.get_session(int(año), gran_premio, 'R')
+        sesion.load(telemetry=True, weather=False)
+
+    except:
+        return "⚠️ Error al buscar."
