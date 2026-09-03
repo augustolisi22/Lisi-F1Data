9c4d2d5eaf4081a80b409d117e28171d657de8ea
augustolisi22 <augustolisi22@gmail.com>
2026-09-02 23:12:17 -0300

    Permite sacar las posiciones de los pilotos, las vueltas totales y la mejor vuelta de cada uno

--- telemetria.py
+++ telemetria.py
@@ -28,5 +28,24 @@ def comparar_ritmo(piloto1, piloto2, gp, año):
         resultado_p1 = sesion.results.loc[sesion.results['Abbreviation'] == p1].iloc[0]
         resultado_p2 = sesion.results.loc[sesion.results['Abbreviation'] == p2].iloc[0]
 
+        try:
+            pos_p1 = f"P{int(resultado_p1['Position'])}"
+        except:
+            pos_p1 = "No se encontró la posición del piloto"
+            
+        try:
+            pos_p2 = f"P{int(resultado_p2['Position'])}"
+        except:
+            pos_p2 = "No se encontró la posición del piloto"
+
+        laps_p1 = sesion.laps.pick_driver(p1)
+        laps_p2 = sesion.laps.pick_driver(p2)
+        
+        vueltas_totales_p1 = len(laps_p1)
+        vueltas_totales_p2 = len(laps_p2)
+        
+        vuelta_p1 = laps_p1.pick_fastest()
+        vuelta_p2 = laps_p2.pick_fastest()
+        
     except:
         return "⚠️ Error al buscar."
