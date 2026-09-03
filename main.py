2fdbc54a6bb412e84918b9a6bc2a2c7c823d8cc8
augustolisi22 <augustolisi22@gmail.com>
2026-09-03 19:40:05 -0300

    Agrego mi funcion para crear mi ventana

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import flet as ft
 from telemetria import comparar_ritmo
 from historial import guardar_busqueda, leer_historial
+
+
+def main(page: ft.Page):
+    page.title = "Lisi-F1-Data"
+    page.theme_mode = "dark" 
