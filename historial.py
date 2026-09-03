8d16d80223acd2d483dc8afeaf31691f12f0f3ee
augustolisi22 <augustolisi22@gmail.com>
2026-09-03 14:05:00 -0300

    Agrego funcion para guardar los resultados de la telemetría

--- historial.py
+++ historial.py
@@ -2,3 +2,12 @@ import csv
 import os
 
 ARCHIVO = "historial.csv"
+
+def guardar_busqueda(p1, p2, gp, año, resultado):
+    try:
+        with open(ARCHIVO, mode='a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([p1, p2, gp, año, resultado])
+    except Exception as e:
+        print(f"Error al guardar historial: {e}")
+        
